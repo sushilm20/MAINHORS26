7f55e2928d2e7534bc72b43f66e5803a8fc0f4f4
Sushil Muthuvelkumar <123318620+sushilm20@users.noreply.github.com>
2025-12-03 22:09:23 -0500

    packaging liek that

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/complexityHORS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/complexityHORS.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.TurretTracker;
+import org.firstinspires.ftc.teamcode.tracking.TurretTracker;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tracking/TurretTracker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tracking/TurretTracker.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.tracking;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
