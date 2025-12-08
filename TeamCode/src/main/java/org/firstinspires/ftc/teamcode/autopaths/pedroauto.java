e31e10d74bc6e8c6e1c1c373bbb57da89e3a6695
Sushil Muthuvelkumar <123318620+sushilm20@users.noreply.github.com>
2025-12-08 00:13:45 -0500

    package change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autopaths/pedroauto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autopaths/pedroauto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.autopaths;
 
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
