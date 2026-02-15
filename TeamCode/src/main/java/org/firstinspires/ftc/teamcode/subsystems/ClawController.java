6b67171e02d6f3591ac367330054c275c1d79b86
Sushil Muthuvelkumar <123318620+sushilm20@users.noreply.github.com>
2026-02-14 23:57:13 -0500

    fix: claw controller loop time/pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ClawController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ClawController.java
@@ -3,9 +3,9 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class ClawController {
-    public static final double CLAW_OPEN = 0.6;
+    public static final double CLAW_OPEN = 0.62;
     public static final double CLAW_CLOSED = 0.3;
-    public static final long CLAW_CLOSE_MS = 500L;
+    public static final long CLAW_CLOSE_MS = 400L;
 
     private final Servo claw;
     private final double openPos;
