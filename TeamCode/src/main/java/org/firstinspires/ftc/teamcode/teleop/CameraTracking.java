3ff70df7d8fb9d1fe48164e9b20afa49332210d9
Sushil Muthuvelkumar <123318620+sushilm20@users.noreply.github.com>
2026-04-03 22:44:39 -0400

    final declare

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/CameraTracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/CameraTracking.java
@@ -255,7 +255,7 @@ public class CameraTracking extends LinearOpMode {
             cameraStreamManager = new CameraStreamManager();
 
             aprilTagProcessor = new AprilTagProcessor.Builder()
-                    .setDrawAxes(true)//sure
+                    .setDrawAxes(true)//bre
                     .setDrawTagOutline(true)
                     .setDrawCubeProjection(true)
                     .setDrawTagID(true)
