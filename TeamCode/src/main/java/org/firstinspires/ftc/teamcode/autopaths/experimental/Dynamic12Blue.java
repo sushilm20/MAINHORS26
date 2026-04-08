aa9c51afbe0feb83fa2c9ed7fb275ab85a6587fa
Sushil Muthuvelkumar <123318620+sushilm20@users.noreply.github.com>
2026-04-07 23:00:33 -0400

    keep old 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autopaths/experimental/Dynamic12Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autopaths/experimental/Dynamic12Blue.java
@@ -88,7 +88,7 @@ public class Dynamic12Blue extends OpMode {
     @Sorter(sort = 3)
     public static double PRE_ACTION_WAIT_SECONDS = 0.35;          // was 1.0 keep old
     @Sorter(sort = 4)
-    public static double PRE_ACTION_MAX_POSE_WAIT_SECONDS = 0.60; // was 1.5
+    public static double PRE_ACTION_MAX_POSE_WAIT_SECONDS = 0.60; // was 1.5 keep old
     @Sorter(sort = 5)
     public static long SHOOTER_WAIT_TIMEOUT_MS = 700L;            // was 1300
 
